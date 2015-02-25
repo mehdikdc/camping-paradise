75e6f2223c0d47de4c7278a1b005690a531a892c
maximebutin <maxime.butin@outlook.com>
2015-02-25 11:50:17 +0100

    Add css/header/footer

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<div id="footer">
+				<h1>FOOTER DE LA PAGE</h1>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,38 @@
+﻿<?php
+$aPage = array(
+	'home'=> array(
+		'Title' => 'Titre de la page',
+		'Description' => 'Description de la page',
+		'Keywords' => 'mots clés',
+	),
+	'portfolio'=> array(
+		'Title' => 'Titre de la page',
+		'Description' => 'Description de la page',
+		'Keywords' => 'mots clés',
+	),
+	'contact'=> array(
+		'Title' => 'Titre de la page',
+		'Description' => 'Description de la page',
+		'Keywords' => 'mots clés',
+	),
+);
+
+?>
+<html>
+	<head>
+		<title><?php echo $aPage[$page]['Title']; ?></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="header">
+			<h1>HEADER DE LA PAGE</h1>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="index.php" >Accueil - </a></li>
+				<li><a href="" >- Déposez les annonces - </a></li>
+				<li><a href="" >- Voir les offres</a></li>
+			</ul>
+		</nav>
+
+
